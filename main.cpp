5e1543eb8947941a852dd5e6496e03d61385b9b2
AissAiss <lucasaissaoui201@gmail.com>
2021-04-13 22:37:48 +0200

    Deux trois modif graphique

--- main.cpp
+++ main.cpp
@@ -6,7 +6,11 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QFile styleSheetFile("D:/L3/S6/GraphesAlgo/Projet/Graphe/Combinear.qss");
+    QFile styleSheetFile("D:/Git/grapheUi/Combinear.qss");
+
+    //Jules D:/L3/S6/GraphesAlgo/Projet/Graphe/Combinear.qss
+    //lucas D:/Git/grapheUi/Combinear.qss
+
     styleSheetFile.open(QFile::ReadOnly);
     QString styleSheet = QLatin1String(styleSheetFile.readAll());
     a.setStyleSheet(styleSheet); // .qss
